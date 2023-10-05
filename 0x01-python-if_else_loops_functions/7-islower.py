4febc6cf04e6ec94c95aa493ca1de3970ec3a298
GMDancun <dantrevor18@gmail.com>
2023-10-05 02:54:50 +0000

    cases

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# a function that prints a str in lowercase
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
         return True
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# a function that prints a str in uppercase
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
